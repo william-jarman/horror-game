cedd6e94f121fa1c061e11f9e85b5f278c2f71c6
lachlanomogonix <lachyesquire456@outlook.com>
2022-12-23 13:17:48 +1100

    Create imWatchingYou.cs

---
+++ horror game/Assets/Scripts/imWatchingYou.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class imWatchingYou : MonoBehaviour
+{
+    //I'm watching you. I can see you looking through the game files.
+    //I can assure you death will be coming your way soon.
+    //Lock your doors and windows.
+    //Because I'm coming.
+}
