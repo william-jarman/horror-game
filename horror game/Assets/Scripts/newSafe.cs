868c0c0071e24346266910d36ec99e8dfa32626f
lachlanomogonix <lachyesquire456@outlook.com>
2023-03-06 15:29:48 +1100

    Prototype Safe (Continued)

--- horror game/Assets/Scripts/newSafe.cs
+++ horror game/Assets/Scripts/newSafe.cs
@@ -8,11 +8,10 @@ using TMPro;
 public class newSafe : MonoBehaviour
 {
     public Transform safeDial;
-    public int correctSafeCode;
     public Animator safeAnimator;
     public GameObject player, safeCamera, intText;
     public bool usingSafe, interactable, toggle;
-    public string currentSafeCode;
+    public string currentSafeCode, correctSafeCode;
     public TextMeshProUGUI safeCodeText;
 
     void OnTriggerStay(Collider other)
@@ -36,6 +35,7 @@ public class newSafe : MonoBehaviour
     }
     void Update()
     {
+        Debug.Log(currentSafeCode);
         if(usingSafe == false)
         {
             if (interactable == true)
@@ -52,64 +52,71 @@ public class newSafe : MonoBehaviour
         }
         if(usingSafe == true)
         {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                safeCamera.SetActive(false);
+                currentSafeCode = "";
+                player.SetActive(true);
+                usingSafe = false;
+                interactable = false;
+            }
             if (Input.GetKeyDown(KeyCode.Alpha1))
             {
-                safeDial.rotation = Quaternion.Euler(-15, 90, 90);
+                safeDial.rotation = Quaternion.Euler(-40, 90, 90);
                 currentSafeCode = currentSafeCode + "1";
             }
             if (Input.GetKeyDown(KeyCode.Alpha2))
             {
-                safeDial.rotation = Quaternion.Euler(-30, 90, 90);
+                safeDial.rotation = Quaternion.Euler(-80, 90, 90);
                 currentSafeCode = currentSafeCode + "2";
             }
             if (Input.GetKeyDown(KeyCode.Alpha3))
             {
-                safeDial.rotation = Quaternion.Euler(-45, 90, 90);
+                safeDial.rotation = Quaternion.Euler(-120, 90, 90);
                 currentSafeCode = currentSafeCode + "3";
             }
             if (Input.GetKeyDown(KeyCode.Alpha4))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, 60, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-160, 90, 90);
                 currentSafeCode = currentSafeCode + "4";
             }
             if (Input.GetKeyDown(KeyCode.Alpha5))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, 75, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-200, 90, 90);
                 currentSafeCode = currentSafeCode + "5";
             }
             if (Input.GetKeyDown(KeyCode.Alpha6))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, safeDial.position.y, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-240, 90, 90);
                 currentSafeCode = currentSafeCode + "6";
             }
             if (Input.GetKeyDown(KeyCode.Alpha7))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, safeDial.position.y, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-280, 90, 90);
                 currentSafeCode = currentSafeCode + "7";
             }
             if (Input.GetKeyDown(KeyCode.Alpha8))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, safeDial.position.y, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-310, 90, 90);
                 currentSafeCode = currentSafeCode + "8";
             }
             if (Input.GetKeyDown(KeyCode.Alpha9))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, safeDial.position.y, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-340, 90, 90);
                 currentSafeCode = currentSafeCode + "9";
             }
             if (Input.GetKeyDown(KeyCode.Alpha0))
             {
-                safeDial.LookAt(new Vector3(safeDial.position.x, safeDial.position.y, safeDial.position.z));
+                safeDial.rotation = Quaternion.Euler(-360, 90, 90);
                 currentSafeCode = currentSafeCode + "0";
             }
             safeCodeText.text = currentSafeCode;
-            string safeCodeString = currentSafeCode;
-            int safeInt = Convert.ToInt32(safeCodeString);
-            if(safeInt == correctSafeCode)
+            if (currentSafeCode.Contains(correctSafeCode))
             {
                 safeCamera.SetActive(false);
                 player.SetActive(true);
                 usingSafe = true;
+                toggle = false;
                 safeAnimator.SetTrigger("open");
             }
         }
